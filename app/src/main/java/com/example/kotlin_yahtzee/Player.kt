f7c9da5ab5c5f7550831443030b0f8b43c69242c
Curtis <kurtedge@googlemail.com>
2025-07-09 16:40:11 +0100

    added calculation of chance and x of a kind

--- app/src/main/java/com/example/kotlin_yahtzee/Player.kt
+++ app/src/main/java/com/example/kotlin_yahtzee/Player.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin_yahtzee
 
+import kotlin.math.max
 import kotlin.random.Random
 
 open class Player (playerName: String){
@@ -19,11 +20,11 @@ open class Player (playerName: String){
     private fun calculateScore(category: Int, diceHand: ArrayList<Int>): Int {
         return when (category + 1) {
             1, 2, 3, 4, 5, 6 -> calculateTopScores(category, diceHand)
-            7, 8 -> 45 //Xofakind
+            7, 8 -> calculateXOfAKindScores(category, diceHand)
             9 -> 25 //full house
             10, 11 -> 40 //straights
             12 -> 50 //yahtzee
-            else -> 3
+            else -> calculateDiceTotalValue(diceHand)
 
         }
     }
@@ -35,8 +36,26 @@ open class Player (playerName: String){
                 score += die
             }
         }
-
         return score
+    }
+
+    private fun calculateXOfAKindScores(category: Int, diceHand: ArrayList<Int>): Int {
+        val maxCount = diceHand
+            .groupingBy { it }
+            .eachCount()
+            .maxOf { it.value }
+
+        return if (category + 1 == 8 && maxCount >= 4) {
+            calculateDiceTotalValue(diceHand)
+        } else if (category + 1 == 7 && maxCount >= 3) {
+            calculateDiceTotalValue(diceHand)
+        } else {
+            0
+        }
+    }
+
+    private fun calculateDiceTotalValue(diceHand: ArrayList<Int>): Int {
+        return diceHand.sum()
     }
 }
 
